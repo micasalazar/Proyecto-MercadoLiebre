75444d41c9669298f048dc1d96f3bd23cc922bb9
micasalazar <micaela.salazar1586@gmail.com>
2023-07-23 09:23:14 -0300

    Diseño Adaptativo- Posicionamiento Flexbox

--- app.js
+++ app.js
@@ -10,3 +10,9 @@ app.listen(3030, () =>{
 app.get('/',(req, res) =>{
     res.sendFile(path.join(__dirname, '/views/home.html'))
 })
+app.get('/register',(req, res) =>{
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+app.get('/login',(req, res) =>{
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
